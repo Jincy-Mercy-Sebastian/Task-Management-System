3cc437eee7e2e2f274dd5b9c528072aa702f7816
Jincy-Mercy-Sebastian <jincyks7777@gmai.com>
2025-01-07 09:10:20 +0530

    Springboot backend code

--- task_management/test_management/src/main/java/com/jthread/test_management/enums/Priority.java
+++
@@ -1,6 +0,0 @@
-package com.jthread.test_management.enums;
-
-public enum Priority {
-	CRITICAL, HIGH, MEDIUM, LOW;
-
-}
--- task_management/test_management/src/main/java/com/jthread/test_management/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.jthread.test_management.exceptions;
-
-public class ResourceNotFoundException extends Exception {
-	public ResourceNotFoundException(String message){
-		super("Resource not found " + message);
-	}
-
-}
